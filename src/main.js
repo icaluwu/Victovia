cbc090d84ee51573abd2b7d7569062dc9f8b24c3
icaluwu <rizdian31@gmail.com>
2025-03-18 12:03:17 +0700

    Adding Icons

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';      // Import Bootstrap CSS
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Import Bootstrap JS
+import 'bootstrap-icons/font/bootstrap-icons.css'; // Import Bootstrap Icon
+
 
 import { createApp } from 'vue';
 import App from './App.vue';
