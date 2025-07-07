059ef586dfc2098683aaad8c0dc50579d3f34714
Teuku Vaickal Rizki Irdian <134307726+icaluwu@users.noreply.github.com>
2025-07-07 14:34:18 +0700

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWeHashbHistory } from 'vue-router';
 import Home from '../pages/Home.vue';
 import About from '../pages/About.vue';
 import Location from '../pages/Location.vue';
